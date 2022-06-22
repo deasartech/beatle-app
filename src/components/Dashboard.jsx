ee08c86b91a0b2051fd14c6e4a3922a3e9b1b074
Dean <86922213+deasartech@users.noreply.github.com>
2022-06-22 03:12:08 +0100

    Revert "remove hearts cookie when game over to fix bug"

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -291,7 +291,7 @@ export default function Dashboard({
 
       setResults([...results, "X"]);
     } else {
-      Cookies.remove("hearts");
+      Cookies.set("hearts", 0, { expires: endOfDay });
       Cookies.set("played", "true", { expires: endOfDay });
       setHearts((currNum) => currNum - 1);
       setTimeout(() => {
