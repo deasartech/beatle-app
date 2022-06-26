afc14812a6c740e0bf02e66ba7ef35722c5db678
deasar-js <deasartechnologies@gmail.com>
2022-06-26 13:34:35 +0100

    add albuma artwork when played, with artist name and track title

--- src/utils/api.js
+++ src/utils/api.js
@@ -4,8 +4,15 @@ const api = axios.create({
   baseURL: "https://api.deezer.com",
 });
 
-export function fetchTrack(trackNum) {
-  return api.get(`/track/${trackNum}`).then(({ data }) => {
-    return data;
-  });
+// export function fetchTrack(trackNum) {
+//   return api.get(`/track/${trackNum}`).then(({ data }) => {
+//     return data;
+//   });
+// }
+export function searchTrack(artist, trackName) {
+  return api
+    .get(`/search?q=artist:"${artist}" track:"${trackName}"`)
+    .then(({ data }) => {
+      return data;
+    });
 }
